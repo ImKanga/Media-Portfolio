9661c5aa329eaa07485a64cc7c7e1bbb760dfab9
ImKanga <iTZaKanga@gmail.com>
2017-10-14 12:17:03 +1000

    Removed custom.js for now (using CSS transitions instead), added jquery-colour (Might be redundant), added nav, played with pictures.

--- custom.js
+++
@@ -1,8 +0,0 @@
-/*$(window).scroll(function(e) {
-    if ($(window).scrollTop() > 150) {
-            $('.header').stop(true, true).fadeOut();
-        } else {
-            $('.header').stop(true, true).fadeIn();
-        }
-});
-*/
